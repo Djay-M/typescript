0ebbbe02e11c6129b21b990d1b289edea2519f46
Dhananajaya <dhananjay.m1805@gmailcom>
2025-07-24 22:43:09 +0530

    added hostName and port

--- apiBasic/src/config/config.ts
+++ apiBasic/src/config/config.ts
@@ -4,3 +4,6 @@ dotenv.config();
 
 export const DEVELOPMENT = process.env.ENV === "development";
 export const TEST = process.env.ENV === "test";
+
+exports.HOST_NAME = process.env.HOST_NAME || "localhost";
+exports.PORT = process.env.PORT || 3000;
