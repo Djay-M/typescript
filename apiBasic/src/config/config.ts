4f546fe3bcf79dbe650d17a80d1c9ca31b60bfb0
Dhananajaya <dhananjay.m1805@gmailcom>
2025-07-24 22:46:49 +0530

    port change to 4000

--- apiBasic/src/config/config.ts
+++ apiBasic/src/config/config.ts
@@ -6,4 +6,4 @@ export const DEVELOPMENT = process.env.ENV === "development";
 export const TEST = process.env.ENV === "test";
 
 exports.HOST_NAME = process.env.HOST_NAME || "localhost";
-exports.PORT = process.env.PORT || 3000;
+exports.PORT = process.env.PORT || 4000;
